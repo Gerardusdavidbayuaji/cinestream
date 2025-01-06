d8c380189680b33f21f82798ed7d816a9f9cca9f
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-01-07 00:02:35 +0700

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,12 @@ const Home = () => {
   return (
     <div>
       <Button>test click</Button>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum ipsam
+        hic, tenetur pariatur voluptatem temporibus eum odit eos neque, vero,
+        voluptatibus nesciunt totam voluptate est similique harum. Ex, animi
+        totam.
+      </p>
     </div>
   );
 };

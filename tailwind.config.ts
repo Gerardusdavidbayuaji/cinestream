edab93bdded91c0c2ec3bbcce5a6285081ba105e
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-01-09 00:27:27 +0700

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,16 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-poppins)", "sans-serif"],

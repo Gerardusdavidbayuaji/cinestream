2fb3f721ef68ce41bc8e7151d01dbf4e24ab68af
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-01-27 09:47:08 +0700

    update: localstorage for saving favorite item

--- src/services/store/favoriteSlice.ts
+++ src/services/store/favoriteSlice.ts
@@ -9,9 +9,25 @@ const initialState: FavoriteState = {
   favorites: [],
 };
 
+const loadFavoritesFromLocalStorage = (): FavoriteState => {
+  if (typeof window !== "undefined") {
+    const savedFavorites = localStorage.getItem("favorites");
+    if (savedFavorites) {
+      return JSON.parse(savedFavorites);
+    }
+  }
+  return initialState;
+};
+
+const saveFavoritesToLocalStorage = (state: FavoriteState) => {
+  if (typeof window !== "undefined") {
+    localStorage.setItem("favorites", JSON.stringify(state));
+  }
+};
+
 export const favoriteSlice = createSlice({
   name: "favorites",
-  initialState,
+  initialState: loadFavoritesFromLocalStorage(),
   reducers: {
     addToFavorite: (state, action: PayloadAction<MovieDetail>) => {
       const exists = state.favorites.find(
@@ -19,12 +35,14 @@ export const favoriteSlice = createSlice({
       );
       if (!exists) {
         state.favorites.push(action.payload);
+        saveFavoritesToLocalStorage(state);
       }
     },
     removeFromFavorite: (state, action: PayloadAction<number>) => {
       state.favorites = state.favorites.filter(
         (movie) => movie.id !== action.payload
       );
+      saveFavoritesToLocalStorage(state);
     },
   },
 });

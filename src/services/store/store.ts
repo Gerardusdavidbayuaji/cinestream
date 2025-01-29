8c7b5077e0c911c4b84f1295a374981b280fa633
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-01-29 20:28:23 +0700

    del: movie slice

--- src/services/store/store.ts
+++ src/services/store/store.ts
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import favoriteReducer from "./favoriteSlice";
-import movieReducer from "./movieSlice";
 
 export const store = configureStore({
   reducer: {
     favorites: favoriteReducer,
-    movies: movieReducer,
   },
 });
 

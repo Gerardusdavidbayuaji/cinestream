5b57d46aa034cd29779880faa47acc3335359755
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-01-30 08:25:12 +0700

    refactor: detail page

--- src/app/movies/detail/[id]/page.tsx
+++ src/app/movies/detail/[id]/page.tsx
@@ -4,26 +4,25 @@ import { getDetailMovie } from "@/services/apis/movies";
 import Detail from "@/modules/movies";
 import Container from "@/components/element/Container";
 
-// export async function generateMetadata({
-//   params,
-// }: {
-//   params: { id: string };
-// }): Promise<Metadata> {
-//   const { id } = params;
-//   const movie = await getDetailMovie(id);
-//   return {
-//     title: movie.title + " | CineStream",
-//   };
-// }
+type DetailMoviePageProps = {
+  params: Promise<{ id: string }>;
+};
+
+export async function generateMetadata({
+  params,
+}: DetailMoviePageProps): Promise<Metadata> {
+  const idMovie = (await params).id;
+  const movie = await getDetailMovie(idMovie);
+  return {
+    title: movie.title + " | CineStream",
+  };
+}
 
 export default async function DetailMoviePage({
   params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const idMovie = await params;
-  const { id } = idMovie;
-  const movie = await getDetailMovie(id);
+}: DetailMoviePageProps) {
+  const idMovie = (await params).id;
+  const movie = await getDetailMovie(idMovie);
 
   return (
     <>

2ee209e12b53e19ad870cb4724356db566378eda
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-01-31 21:58:22 +0700

    refactor: detail page

--- src/app/movies/detail/[id]/page.tsx
+++ src/app/movies/detail/[id]/page.tsx
@@ -1,43 +1,34 @@
 import { Metadata } from "next";
-import { notFound } from "next/navigation";
 
 import { getDetailMovie } from "@/services/apis/movies";
 import Detail from "@/modules/movies";
 import Container from "@/components/element/Container";
 
-// Define the type for Detail Page Props
 type DetailMoviePageProps = {
-  params: { id: string } | Promise<{ id: string }>;
+  params: Promise<{ id: string }>;
 };
 
-// Generate Metadata for SEO
 export async function generateMetadata({
   params,
 }: DetailMoviePageProps): Promise<Metadata> {
-  const resolvedParams = await params;
-  const movie = await getDetailMovie(resolvedParams.id);
-  if (!movie) return {};
-
+  const idMovie = (await params).id;
+  const movie = await getDetailMovie(idMovie);
   return {
-    title: `${movie.title} | CineStream`,
+    title: movie.title + " | CineStream",
   };
 }
 
-// Detail Movie Page Component
 export default async function DetailMoviePage({
   params,
 }: DetailMoviePageProps) {
-  const resolvedParams = await params;
-  const movie = await getDetailMovie(resolvedParams.id);
-
-  // Handle Not Found Case
-  if (!movie) {
-    return notFound();
-  }
+  const idMovie = (await params).id;
+  const movie = await getDetailMovie(idMovie);
 
   return (
-    <Container>
-      <Detail movie={movie} />
-    </Container>
+    <>
+      <Container>
+        <Detail movie={movie} />
+      </Container>
+    </>
   );
 }
